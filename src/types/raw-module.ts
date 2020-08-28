1f666da671bf6a8cf35783dcf6cae7327fb7c3e4
Mohamed Salem <salem.ebo@gmail.com>
2020-08-28 15:45:11 +0200

    add more raw formats

--- src/types/raw-module.ts
+++ src/types/raw-module.ts
@@ -12,19 +12,38 @@ declare module '*.txt' {
     const value: string
     export default value
 }
+
 declare module '*.json' {
     const value: any
     export default value
 }
+
 declare module '*.b64' {
     const value: string
     export default value
 }
+
 declare module '*.buf' {
     const value: Uint8Array
     export default value
 }
+
 declare module '*.css' {
     const value: string
     export default value
+}
+
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.jpg" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: any;
+    export default content;
 }
